4582480504d81ba869071432195286282fb8e3b0
vrk <vrkwork@gmail.com>
2016-08-26 16:51:49 -0700

    mid point

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header'
+import NoteNavigation from './NoteNavigation'
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <NoteNavigation />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
