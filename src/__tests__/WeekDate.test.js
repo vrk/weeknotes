d8b32c0cc851bd12edbd0524ee83d13f085bf758
vrk <vrk@twooey.stanford.edu>
2016-09-01 17:56:30 -0700

    tested ranges

--- src/__tests__/WeekDate.test.js
+++ src/__tests__/WeekDate.test.js
@@ -1,13 +1,36 @@
 import WeekDate from '../WeekDate';
 
-const SAT_SEPT_10_2016 = new Date('Sat Sep 10 2016');
+const SUN_DEC_27_2015 = new Date('Sun Dec 27 2015');
+const MON_DEC_28_2015 = new Date('Mon Dec 28 2015');
+const FRI_JAN_1_2016 = new Date('Fri Jan 01 2016');
+const SAT_JAN_2_2016 = new Date('Sat Jan 02 2016');
+const SUN_AUG_28_2016 = new Date('Sun Aug 28 2016');
+const TUE_AUG_30_2016 = new Date('Tue Aug 30 2016');
+const THU_SEPT_1_2016 = new Date('Thu Sep 01 2016');
+const SAT_SEPT_3_2016 = new Date('Sat Sep 03 2016');
 const SUN_SEPT_4_2016 = new Date('Sun Sep 04 2016');
 const MON_SEPT_5_2016 = new Date('Mon Sep 05 2016');
+const SAT_SEPT_10_2016 = new Date('Sat Sep 10 2016');
 
 describe('getStartOfRange() tests', () => {
   it('start range works when sunday', () => {
     const weekdate = new WeekDate(SUN_SEPT_4_2016);
     expect(weekdate.getStartOfRange()).toEqual(SUN_SEPT_4_2016);
+  });
+
+  it('start range works when middle of week', () => {
+    const weekdate = new WeekDate(MON_SEPT_5_2016);
+    expect(weekdate.getStartOfRange()).toEqual(SUN_SEPT_4_2016);
+  });
+
+  it('start range works across months', () => {
+    const weekdate = new WeekDate(THU_SEPT_1_2016);
+    expect(weekdate.getStartOfRange()).toEqual(SUN_AUG_28_2016);
+  });
+
+  it('start range works across years', () => {
+    const weekdate = new WeekDate(FRI_JAN_1_2016);
+    expect(weekdate.getStartOfRange()).toEqual(SUN_DEC_27_2015);
   });
 });
 
@@ -15,5 +38,20 @@ describe('getEndOfRange() tests', () => {
   it('end range works when saturday', () => {
     let weekdate = new WeekDate(SAT_SEPT_10_2016);
     expect(weekdate.getEndOfRange()).toEqual(SAT_SEPT_10_2016);
+  });
+
+  it('end range works when middle of week', () => {
+    const weekdate = new WeekDate(MON_SEPT_5_2016);
+    expect(weekdate.getEndOfRange()).toEqual(SAT_SEPT_10_2016);
+  });
+
+  it('end range works across months', () => {
+    const weekdate = new WeekDate(TUE_AUG_30_2016);
+    expect(weekdate.getEndOfRange()).toEqual(SAT_SEPT_3_2016);
+  });
+
+  it('end range works across years', () => {
+    const weekdate = new WeekDate(MON_DEC_28_2015);
+    expect(weekdate.getEndOfRange()).toEqual(SAT_JAN_2_2016);
   });
 });
