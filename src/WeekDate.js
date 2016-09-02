e2d0fbb46e0259836c2dabdb8d615d2abd0b95bf
vrk <vrk@twooey.stanford.edu>
2016-09-01 17:36:36 -0700

    wooooo adding tests

---
+++ src/WeekDate.js
@@ -0,0 +1,27 @@
+class WeekDate {
+  constructor(date) {
+    this.date = date || new Date();
+  }
+
+  getStartOfRange() {
+    let newDate = new Date(this.date);
+    newDate.setDate(this.date.getDate() - this.date.getDay());
+    return newDate;
+  }
+
+  getEndOfRange() {
+    let newDate = new Date(this.date);
+    newDate.setDate(this.date.getDate() - this.date.getDay() + 6);
+    return newDate;
+  }
+
+  incrementWeek() {
+    this.date.setDate(this.date.getDate() + 7);
+  }
+
+  decrementWeek() {
+    this.date.setDate(this.date.getDate() - 7);
+  }
+}
+
+export default WeekDate;
---
+++ src/__tests__/WeekDate.test.js
@@ -0,0 +1,15 @@
+import WeekDate from '../WeekDate';
+
+const SAT_SEPT_10_2016 = new Date("Sat Sep 10 2016");
+const SUN_SEPT_4_2016 = new Date("Sun Sep 04 2016");
+const MON_SEPT_5_2016 = new Date("Mon Sep 05 2016");
+
+it('start range works when sunday', () => {
+  const weekdate = new WeekDate(SUN_SEPT_4_2016);
+  expect(weekdate.getStartOfRange()).toEqual(SUN_SEPT_4_2016);
+});
+
+it('end range works when saturday', () => {
+  let weekdate = new WeekDate(SAT_SEPT_10_2016);
+  expect(weekdate.getEndOfRange()).toEqual(SAT_SEPT_10_2016);
+});
