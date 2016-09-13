5a08879dddfea26c4762f90f1e717491eb61edfb
vrk <vrkwork@gmail.com>
2016-09-13 01:06:19 -0700

    trying

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 app.set('port', (process.env.API_PORT || 3001));
 
 app.get('/api/notes', (req, res) => {
+  console.log('here!');
   const param = req.query.q;
 
   if (!param) {
@@ -20,5 +21,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.listen(app.get('port'), () => {
+  console.log(process);
+  console.log(__dirname);
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
