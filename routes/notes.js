d81368d77e53d72bd13e4540ed09514a2444dcb2
vrk <vrkwork@gmail.com>
2016-09-15 10:30:46 -0700

    add validate lib

--- routes/notes.js
+++ routes/notes.js
@@ -1,54 +1,14 @@
 var express = require('express');
+var { validateToken } = require('../lib/auth');
+
 var router = express.Router();
 
 router.get('/notes', (req, res) => {
-  let map = [];
-  map[0] = {'yo': 'hello'};
-  res.json(map);
+  validateToken('fake_token', (user_info) => {
+    console.log(user_info);
+    let map = [];
+    map[0] = user_info;
+    res.json(map);
+  });
 });
-
-  /*
-  const param = req.query.q;
-
-  if (!param) {
-    res.json({
-      error: 'Missing required parameter `q`',
-    });
-    return;
-  }
-
-  var postData = querystring.stringify({
-    id_token: param
-  });
-
-  var options = {
-    hostname: 'www.googleapis.com',
-    path: '/oauth2/v3/tokeninfo',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(postData)
-    }
-  };
-
-  var req = https.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-    res.on('end', () => {
-      console.log('No more data in response.');
-    });
-  });
-
-  req.on('error', (e) => {
-    console.log(`problem with request: ${e.message}`);
-  });
-
-  // write data to request body
-  req.write(postData);
-  req.end();
-  */
 module.exports = router;
