bc8e79b6d7a9ac18d84f7725cfec4dd27406d5b5
vrk <vrkwork@gmail.com>
2016-09-15 10:56:19 -0700

    .

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-var notes = require('./routes/notes');
+var notes_router = require('./routes/notes');
 var { Notes } = require('./models/notes');
 var { Users } = require('./models/users');
 
@@ -8,17 +8,19 @@ const app = express();
 
 app.set('port', (process.env.PORT || 3001));
 
+// DUMMY DB
 let db = {};
 let users = new Users(db);
+let notes = new Notes(db);
 
 // Middleware to give requests access to the Users and Notes models.
 app.use((req, res, next) => {
-  req.notes = notes;
   req.users = users;
+  req.notes = notes;
   next();
 });
 
-app.use('/api', notes);
+app.use('/api', notes_router);
 
 if ( app.get('env') !== 'development' ) {
   app.use(express.static(path.join(__dirname, 'client/build')));
