d5cf0f473eb0cb7beabd740de49dceabade93152
vrk <vrkwork@gmail.com>
2016-09-17 13:58:03 -0700

    maybe this works?

--- server.js
+++ server.js
@@ -7,8 +7,22 @@ var notes_router = require('./routes/notes');
 const app = express();
 
 if (app.get('env') === 'development') {
+  // Dev-only.
+  // TODO(vrk): This seems like the wrong thing to do. There's probably some way I
+  // can get the same behavior without having to add this code, e.g. using some
+  // command-line flags in the "npm run" script.
   const dotenv = require('dotenv');
   dotenv.config();
+} else {
+  // Prod-only.
+  // TODO(vrk): Should probably move this to a proper middleware file.
+  app.get('*', (req, res, next) => {
+		if (req.headers['x-forwarded-proto'] !== 'https') {
+			res.redirect('https://' + req.url);
+		} else {
+			next();
+    }
+	});
 }
 
 app.set('port', (process.env.PORT || 3100));
