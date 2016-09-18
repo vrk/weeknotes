8f26c7dc806ae8680f07faf760a2ca24f576df3c
vrk <vrkwork@gmail.com>
2016-09-18 01:27:43 -0700

    add default note

--- client/src/reducers/AppReducers.js
+++ client/src/reducers/AppReducers.js
@@ -1,6 +1,7 @@
 /* @flow */
 
 import { OAUTH_LOADED, LOGIN, LOGOUT, LOCAL_NOTE_UPSERT } from '../actions/WeekNoteActions'
+import WeekDate from '../lib/WeekDate'
 
 // TODO(vrk): Change notes to a Map?
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
@@ -28,11 +29,18 @@ const initialState = {
   // Whether the week note editor is active.
   auth: null,
   currentUser: null,
-  notes: {}
+  notes: getDefaultNote()
 };
 
-function convertNotesToMap(notes_response) {
-  var notes_map = {};
+function getDefaultNote() {
+  let note_map = {};
+  const week_id = new WeekDate().getId();
+  note_map[week_id] = "# Welcome to Weeknotes!\n- Click to edit your note!\n- Use [Markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) for formatting";
+  return note_map;
+}
+
+function convertNotesToMap(state, notes_response) {
+  var notes_map = Object.assign({}, state.notes);
   for (const note of notes_response) {
     notes_map[note.week_id] = note.contents;
   }
@@ -41,17 +49,18 @@ function convertNotesToMap(notes_response) {
  
 export default function rootReducer(
   state: State = initialState, action: Action) {
+  console.log(state);
   switch (action.type) {
     case OAUTH_LOADED:
       return Object.assign({}, state, { 
         auth: action.auth,
         currentUser: action.user || null,
-        notes: convertNotesToMap(action.notes)
+        notes: convertNotesToMap(state, action.notes)
       });
     case LOGIN:
       return Object.assign({}, state, {
         currentUser: action.user,
-        notes: convertNotesToMap(action.notes)
+        notes: convertNotesToMap(state, action.notes)
       });
     case LOGOUT:
       return Object.assign({}, state, {
