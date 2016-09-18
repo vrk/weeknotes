acbd2eaf62186742407359536f38c55e92b34890
vrk <vrkwork@gmail.com>
2016-09-17 19:23:32 -0700

    add types for reducers

--- client/src/reducers/AppReducers.js
+++ client/src/reducers/AppReducers.js
@@ -1,4 +1,28 @@
+/* @flow */
+
 import { OAUTH_LOADED, LOGIN, LOGOUT, LOCAL_NOTE_UPSERT } from '../actions/WeekNoteActions'
+
+// TODO(vrk): Change notes to a Map?
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
+type Note = {
+  contents: string,
+  week_id: string
+}
+
+type Action = {
+  type: string,
+  user: ?Object,
+  note: Note,
+  notes: Note[],
+  auth: Object
+};
+
+type State = {
+  auth: ?Object,
+  currentUser: ?Object,
+  notes: {[id:string]: string}
+};
 
 const initialState = {
   // Whether the week note editor is active.
@@ -15,7 +39,8 @@ function convertNotesToMap(notes_response) {
   return notes_map;
 }
  
-export default function rootReducer(state = initialState, action) {
+export default function rootReducer(
+  state: State = initialState, action: Action) {
   switch (action.type) {
     case OAUTH_LOADED:
       return Object.assign({}, state, { 
